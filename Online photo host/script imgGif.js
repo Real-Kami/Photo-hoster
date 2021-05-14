03e845aac7991285f2ab7367d33adc13f8c60b2c
Architect-0 <67558858+Architect-0@users.noreply.github.com>
2021-05-14 19:20:29 +0200

    Update script imgGif.js

--- Online photo host/script imgGif.js
+++ Online photo host/script imgGif.js
@@ -1,7 +1,4 @@
-console.log("Hello World");
-
 let container = document.getElementById("imgGif");
-
 let PhotoList = ["CLC/OT7 5.jpg",
 "CLC/OT7 3.jpg",
 "CLC/OT7 Devil.jpg",
@@ -255,7 +252,7 @@ container.innerHTML = "<img id=\"imgGifContent\" src=\"../" + PhotoList[r] + "\"
 function clickGif() {
 r++;
 if (r > PhotoList.length) {
-r = 0;
+    r = 0;
 }
 container.innerHTML = "<img id=\"imgGifContent\" src=\"../" + PhotoList[r] + "\"/>";
 }
